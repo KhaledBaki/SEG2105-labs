9667df9e73f22a32278b7027fd18e2f281259020
KhaledBaki <khaledbaki2018@gmail.com>
2026-05-21 14:29:08 -0400

    branching out of main

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World, I am version 2.0 of the program");
+		System.out.println("Hello World, I am version 2.0 of the program, I am on the branch conflict-demo");
 	}
 }
