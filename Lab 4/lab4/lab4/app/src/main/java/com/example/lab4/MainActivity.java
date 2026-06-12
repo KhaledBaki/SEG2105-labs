375613094f9108d4131e158c8e9e00bc31f272f8
KhaledBaki <khaledbaki2018@gmail.com>
2026-06-12 16:37:59 -0400

    Updated Lab4 code

--- Lab 4/lab4/lab4/app/src/main/java/com/example/lab4/MainActivity.java
+++ Lab 4/lab4/lab4/app/src/main/java/com/example/lab4/MainActivity.java
@@ -44,9 +44,9 @@ public class MainActivity extends AppCompatActivity {
 
         super.onStart();
         // Attatching value event listener
-        databaseProducts.addValueEventListener(new ValueEventListener()) {
+        databaseProducts.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange (DataSnapshot dataSnapshot){
+            public void onDataChange (DataSnapshot dataSnapshot) {
                 // Clearing the previous artist list
                 products.clear();
 
@@ -62,39 +62,62 @@ public class MainActivity extends AppCompatActivity {
                 ProductList productsAdapter = new ProductList(MainActivity.this, products);
                 //attaching adapter to the listview
                 listViewProducts.setAdapter(productsAdapter);
-
-                //attaching value event listener
-                databaseProducts.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-
-                        //clearing the previous artist list
-                        products.clear();
-
-                        //iterating through all the nodes
-                        for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
-                            //getting product
-                            Product product = postSnapshot.getValue(Product.class);
-                            //adding product to the list
-                            products.add(product);
-                        }
-                        //creating adapter
-                        ProductList productsAdapter = new ProductList(MainActivity.this, products);
-                        //attaching adapter to the listview
-                        listViewProducts.setAdapter(productsAdapter);
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-                });
-
             }
             @Override
-            public void onCancelled (DatabaseError databaseError){
-
+            public void onCancelled(DatabaseError databaseError) {
             }
+        });
+    }
+
+    protected void addProduct(){
+        //getting the value to save
+        String name = editTextName.getText().toString.trim();
+        double price = Double.parseDouble(String.valueOf(editTextPrice.getText().toString()));
+
+        // Checking if the value is provided
+        if(!TextUtils.isEmpty(name)){
+
+            // getting a unique id using push().getKey() method
+            // it will create a unique id and we will use it as the Primary key for our product
+            String id = databaseProducts.push().getKey();
+
+            // Creating a product object
+            Product product = new Product(id, name, price);
+
+            // Saving the product
+            databaseProducts.child(id).setValue(product);
+
+            // setting the edittext to blank again
+            editTextName.setText("");
+            editTextPrice.setText("");
+
+            // Displaying a success toast
+            Toast.makeText(this, "Product added", Toast.LENGTH_LONG).show();
+        } else {
+            // if the  value is not given displaying a toast
+            Toast.makeText(this, "Please enter a name", Toast.LENGTH_LONG).show();
         }
     }
+
+    private void updateProduct(String id, String name, double price){
+        // getting the specified product reference
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("products").child(id);
+
+        // updating product
+        Product product = new Product(id, name, price);
+        databaseReference.setValue(product);
+
+        Toast.makeText(getApplicationContext(), "Product Updated", Toast.LENGTH_LONG).show();
+    }
+
+    private boolean deleteProduct(String id) {
+        // Getting the specified product reference
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("products").child(id);
+
+        // removing product
+        databaseReference.removeValue();
+        Toast.makeText(getApplicationContext(), "Product Deleted", Toast.LENGTH_LONG).show();
+        return true;
+    }
 }
+
