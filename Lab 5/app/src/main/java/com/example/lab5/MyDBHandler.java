6a9fac751328b1c0dccb023355227aa1b1a7638a
KhaledBaki <khaledbaki2018@gmail.com>
2026-06-22 13:14:48 -0400

    DEMONSTRATED THE LAB TO THE TA + UPDATED LAB 5 UI

---
+++ Lab 5/app/src/main/java/com/example/lab5/MyDBHandler.java
@@ -0,0 +1,41 @@
+package com.example.lab5;
+
+import android.content.Context;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MyDBHandler extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "productDB.db";
+    public static final String TABLE_PRODUCTS = "products";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_PRODUCTNAME = "productname";
+    public static final String COLUMN_SKU = "SKU";
+
+    // Constructor
+    public MyDBHandler(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_PRODUCTS_TABLE = "CREATE TABLE " + TABLE_PRODUCTS + " ("
+                + COLUMN_ID + " INTEGER PRIMARY KEY, "
+                + COLUMN_PRODUCTNAME + " TEXT, "
+                + COLUMN_SKU + " INTEGER" + ")";
+        db.execSQL(CREATE_PRODUCTS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PRODUCTS);
+        onCreate(db);
+    }
+
+    public void addProduct(Product product){
+
+    }
+
+}
---
+++ Lab 5/app/src/main/java/com/example/lab5/Product.java
@@ -0,0 +1,41 @@
+package com.example.lab5;
+public class Product {
+    private int productID;
+    private String name;
+    private int SKU;
+
+    /**
+     * Product Constructor
+     *
+     * @param productID identification of product
+     * @param name of product
+     * @param SKU of product
+     */
+    public Product(int productID, String name, int SKU){
+        this.productID = productID;
+        this.name = name;
+        this.SKU = SKU;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getSKU() {
+        return SKU;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSKU(int SKU) {
+        this.SKU = SKU;
+    }
+}
